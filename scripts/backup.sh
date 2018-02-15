03a52d845352efdae30707c8a699364358e3c553
Infosec Alchemist <alchemist@purplelabs.io>
2018-02-14 19:11:08 -0500

    scripts to update and restore from the volumes'

--- scripts/backup.sh
+++ scripts/backup.sh
@@ -1,8 +1,14 @@
-echo "api-config"
-docker run -it -v unfetter_api-config:/data -v /tmp/backup/:/backup alpine tar -cvf /backup/api-config.tar -C /data/ .
-echo "socket-config"
-docker run -it -v unfetter_socket-config:/data -v /tmp/backup/:/backup alpine tar -cvf /backup/socket-config.tar -C /data/ .
-echo "data-db"
-docker run -it -v unfetter_data-db:/data -v /tmp/backup/:/backup alpine tar -cvf /backup/data-db.tar -C /data/ .
+#echo "api-config"
+#docker run -it -v unfetter_api-config:/data -v /tmp/backup/:/backup alpine tar -cvf /backup/api-config.tar -C /data/ .
+
+#echo "socket-config"
+#docker run -it -v unfetter_socket-config:/data -v /tmp/backup/:/backup alpine tar -cvf /backup/socket-config.tar -C /data/ .
+
 echo "certs"
 docker run -it -v unfetter_certs:/data -v /tmp/backup/:/backup alpine tar -cvf  /backup/cert.tar -C /data/ .
+
+echo "backup mongo"
+docker exec -it cti-stix-store-repository mongodump --host localhost --archive=/data/backup/mongo.archive
+
+echo "backup mongo"
+docker run -it -v unfetter_data-backup:/data -v /tmp/backup/:/backup alpine cp /data/mongo.archive /backup/mongo.archive
--- scripts/restore.sh
+++ scripts/restore.sh
@@ -1,8 +1,16 @@
-echo "api-config"
-docker run -it -v unfetter_api-config:/data -v /tmp/backup/:/backup alpine tar -xvf /backup/api-config.tar -C /data/ .
-echo "socket-config"
-docker run -it -v unfetter_socket-config:/data -v /tmp/backup/:/backup alpine tar -xvf /backup/socket-config.tar -C /data/ .
-echo "data-db"
-docker run -it -v unfetter_data-db:/data -v /tmp/backup/:/backup alpine tar -xvf /backup/data-db.tar -C /data/ .
+
+#echo "api-config"
+#docker run -it -v unfetter_api-config:/data -v /tmp/backup/:/backup alpine tar -xvf /backup/api-config.tar -C /data/ .
+
+#echo "socket-config"
+#docker run -it -v unfetter_socket-config:/data -v /tmp/backup/:/backup alpine tar -xvf /backup/socket-config.tar -C /data/ .
+
 echo "certs"
 docker run -it -v unfetter_certs:/data -v /tmp/backup:/backup alpine tar -xvf /backup/cert.tar -C /data/ .
+
+
+echo "copy mongo"
+docker run -it -v unfetter_data-backup:/data -v /tmp/backup/:/backup alpine cp /backup/mongo.archive /data/mongo.archive
+
+echo "restore mongo"
+docker exec -it cti-stix-store-repository mongorestore --host localhost --archive=/data/backup/mongo.archive
